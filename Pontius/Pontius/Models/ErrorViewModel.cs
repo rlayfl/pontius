d028248305b89e40789db2cf8a92df46c73437a0
rlayfl <rlayfl@essex.ac.uk>
2025-08-15 12:37:47 +0100

    Initial setup

---
+++ Pontius/Pontius/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pontius.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
